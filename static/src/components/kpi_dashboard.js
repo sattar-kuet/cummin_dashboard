facac89c1c92b9c6dc7db5c9b151ff173336ec70
Naznin Akter <naznin.kuet.te@gmail.com>
2024-01-24 22:37:40 +0600

    send data from js

--- static/src/components/kpi_dashboard.js
+++ static/src/components/kpi_dashboard.js
@@ -3,11 +3,29 @@
 import { registry } from "@web/core/registry"
 import { KpiCard } from "./kpi_card/kpi_card"
 import { loadJS } from "@web/core/assets"
-const { Component, onWillStart, useRef, onMounted } = owl
+const { Component, onWillStart, useRef, onMounted, useState } = owl
 
 export class OwlKpiDashboard extends Component {
     setup() {
+        /* #####################################################################################
+                                 ATTENTION: such initial config is MUST
+        ########################################################################################*/
+        this.state = useState({
+            period: 90,
+        })
+        onWillStart(async () => {
+            this.loadKpiData()
+        })
+    }
 
+    loadKpiData() {
+        let growthMindSet = {
+            woCount: {
+                open: 100,
+                invoiced: 110
+            }
+        }
+        this.state.growthMindSet = growthMindSet
     }
 }
 

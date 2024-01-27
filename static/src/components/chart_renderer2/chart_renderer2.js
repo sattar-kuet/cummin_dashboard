cfb355122a8aa1092cc3a5a3184ac86273a1de14
Naznin Akter <naznin.kuet.te@gmail.com>
2024-01-27 12:09:37 +0600

    code refactor

--- static/src/components/chart_renderer2/chart_renderer2.js
+++ static/src/components/chart_renderer2/chart_renderer2.js
@@ -10,7 +10,6 @@ export class ChartRenderer2 extends Component {
         onWillStart(async () => {
             await loadJS("https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js")
         })
-
         onMounted(() => this.renderChart2())
     }
     renderChart2() {
@@ -18,16 +17,23 @@ export class ChartRenderer2 extends Component {
         const tb_data = this.props.data;
         new Chart(this.chartRef.el,
             {
-                type: this.props.type,
                 data: {
-                    labels: tb_data?.map((row) => row.date),
                     datasets: [
                         {
+                            type: "line",
+                            // label: "Line Dataset",
                             data: tb_data?.map((row) => row.count),
-                            borderWidth: 1,
-                            backgroundColor: [this.props.bg_color],
+                            backgroundColor: ["#EE2E24"],
+                            borderColor: ["#EE2E24"],
+                        },
+                        {
+                            type: "bar",
+                            // label: "Bar Dataset",
+                            data: tb_data?.map((row) => row.count),
+                            backgroundColor: ["#DB843D"],
                         },
                     ],
+                    labels: tb_data?.map((row) => row.date),
                 },
                 options: {
                     scales: {
@@ -35,6 +41,7 @@ export class ChartRenderer2 extends Component {
                             beginAtZero: true,
                         },
                     },
+
                     plugins: {
                         title: {
                             display: true,
@@ -53,4 +60,5 @@ export class ChartRenderer2 extends Component {
         );
     }
 }
+
 ChartRenderer2.template = "owl.ChartRenderer2"

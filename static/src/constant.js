81f6f6e62151884943d6c8f452b67bcde2c27136
Naznin Akter <naznin.kuet.te@gmail.com>
2024-02-16 04:42:28 +0600

    one data is dynamic in drilldown

---
+++ static/src/constant.js
@@ -0,0 +1,15 @@
+/** @odoo-module */
+export class Constant {
+    filteringParam = {
+        distributorId: 0,
+        distributor: '',
+        period: '',
+        showDateRange: false,
+        periodStartAt: '',
+        periodEndAt: '',
+        country: '',
+        branch: '',
+        currency: '',
+        showTbDetail: false
+    }
+}

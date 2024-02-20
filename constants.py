8fc410ceb7c2a31af69a8f63abc539028c51a735
Naznin Akter <naznin.kuet.te@gmail.com>
2024-02-20 15:43:45 +0600

    push update

---
+++ constants.py
@@ -0,0 +1,22 @@
+#  ('0101', 'Idle time'),
+#         ('0102', 'Internal Administration'),
+#         ('0103', 'Parts Delay'),
+#         ('0104', 'Technical Challenge'),
+#         ('0105', 'Tool Challenge'),
+#         ('0106', 'Training'),
+#         ('0107', 'Travel NB'),
+#         ('0108', 'Onsite Delay'),
+#         ('0109', 'Awaiting Instructions'),
+#         ('0300', 'Maternity Leave'),
+#         ('0410', 'Study Leave'),
+#         ('0710', 'Authorized absence'),
+#         ('0711', 'Unauthorized absence'),
+#         ('0712', 'Sick Leave'),
+#         ('0800', 'Productive Hours'),
+
+PRODUCTIVE_HOURS = ['0104','0105','0107','0108','0800']
+NON_PRODUCTIVE_HOURS = ['0101','0102','0109']
+NON_PRODUCTIVE_HOURS = ['0300','0410','0710','0711','0712']
+TRAINING_HOURS = ['0106']
+
+

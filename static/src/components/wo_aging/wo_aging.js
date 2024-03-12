a980aa4c8eedf395edd7428a1747b7494de81176
Naznin Akter <naznin.kuet.te@gmail.com>
2024-03-12 14:06:15 +0600

    activate live url

--- static/src/components/wo_aging/wo_aging.js
+++ static/src/components/wo_aging/wo_aging.js
@@ -89,8 +89,8 @@ export class WoAging extends Component {
         utility.setCookie('wip_detail_params', filteringDataString, 30)
         // this.actionService.doAction("cummin_dashboard.action_wip_detail")
         let baseUrl = utility.getBaseUrl()
-        // let redirectTo = baseUrl + '/web#action=962&cids=1&menu_id=805' // LIVE
-        let redirectTo = baseUrl + '/web#action=942&cids=1&menu_id=780' // Local
+        let redirectTo = baseUrl + '/web#action=962&cids=1&menu_id=805' // LIVE
+        // let redirectTo = baseUrl + '/web#action=942&cids=1&menu_id=780' // Local
         window.location.href = redirectTo
 
     }

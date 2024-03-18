12b264b21831186755229baeaaf1dc2b68d18ff1
Naznin Akter <naznin.kuet.te@gmail.com>
2024-03-18 20:33:48 +0600

    debug

--- controllers/main.py
+++ controllers/main.py
@@ -132,9 +132,9 @@ class Api(http.Controller):
         maintenance_request_open_ids = []
         maintenance_request_invoiced_ids = []
         stage_id = request.env['maintenance.stage'].search([('name','=','Job Closure')],limit=1).id
-        return stage_id
+        # return stage_id
         for maintenance_request in maintenance_requests:
-            
+            return maintenance_request.stage_id
             if maintenance_request.stage_id == stage_id:
               return 'OK3'
               wo_invoiced += 1
